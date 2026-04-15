b96cf47e57874c2ff143860c013bf3dcf37eff04
TyAtai <zhitai_jin@outlook.com>
2026-04-15 10:05:02 -0400

    deploy using create-react-app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import TestComponent from "./components/TestComponent";
+import {useState} from "react";
 
 const Comp = () => {
   return <p>This is a comp component</p>
 }
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App" id='root'>
             <TestComponent />
 
       <header className="App-header">
+        <button
+          onClick={() => setCount(count + 1)}
+        >Clicks: {count}</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
